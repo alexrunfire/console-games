328f521c01d85cd69aa6b04de14e452e8332beca
Alex <severtabwestone@gmail.com>
2020-01-17 22:31:00 +0300

    rename functions and constants, improve function return index.js

--- src/index.js
+++ src/index.js
@@ -2,26 +2,33 @@ import readlineSync from 'readline-sync';
 
 import { car, cdr } from '@hexlet/pairs';
 
-const showQuestion = (name, getQuestionOrAnswer, counter = 0) => {
-  if (counter === 3) {
-    return console.log(`Congratulations, ${name}!`);
-  }
-  const questionAndAnswer = getQuestionOrAnswer();
-  const question = car(questionAndAnswer);
-  console.log(`Question: ${question}`);
-  const userAnswer = readlineSync.question('Your answer: ');
-  const correctAnswer = cdr(questionAndAnswer);
-  if (userAnswer === correctAnswer) {
-    console.log('Correct!');
-    return showQuestion(name, getQuestionOrAnswer, counter + 1);
-  }
-  return console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
-};
-export default (gameDescription, getRandom) => {
+const numbersOfCycles = 3;
+
+export default (gameDescription, getQuestionAndAnswer) => {
   console.log();
   console.log('Welcome to the Brain-Games!');
   console.log(`${gameDescription}\n`);
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!\n`);
-  return showQuestion(userName, getRandom);
+
+  const showQuestionAndCheckAnswer = (counter = 0) => {
+    if (counter === numbersOfCycles) {
+      console.log(`Congratulations, ${userName}!`);
+      return;
+    }
+
+    const questionAndAnswer = getQuestionAndAnswer();
+    const question = car(questionAndAnswer);
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    const correctAnswer = cdr(questionAndAnswer);
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+      showQuestionAndCheckAnswer(counter + 1);
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
+    }
+  };
+
+  showQuestionAndCheckAnswer();
 };
